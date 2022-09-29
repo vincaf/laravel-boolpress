1f671c3583a01df598edbde96b45691d063f6518
vincaf <vincenzocafassog@gmail.com>
2022-09-29 17:14:57 +0200

    feat(front.js): add router in front.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,12 +1,14 @@
-window.Vue = require('vue');
-
-Vue.use(VueRouter)
-
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './views/App';
 
+window.Vue = require('vue');
+Vue.use(VueRouter)
+
+import router from './router';
+
 const app = new Vue({
     el: '#root',
-    render: h => h(App) //mostriamo App all'avvio di Vue
-})
+    render: h => h(App), //mostriamo App all'avvio di Vue
+    router
+});
